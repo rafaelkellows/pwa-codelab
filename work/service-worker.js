beddd7ed25c4f75e2522b714df3e6f1704e1f871
Rafael Kellows <rafaelkellows@hotmail.com>
2017-03-17 18:55:34 -0300

    Update PWA

--- work/service-worker.js
+++ work/service-worker.js
@@ -1,3 +1,4 @@
+var dataCacheName = 'weatherData-v1';
 var cacheName = 'weatherPWA-step-6-1';
 var filesToCache = [
 	'/',
@@ -18,7 +19,7 @@ var filesToCache = [
 	'/images/thunderstorm.png',
 	'/images/wind.png'
 ];
-
+/* install */
 self.addEventListener('install', function(e){
 	console.log('[ServiceWorker] Install');
 	e.waitUntil(
@@ -29,13 +30,13 @@ self.addEventListener('install', function(e){
 			})
 	)
 });
-
+/* activate */
 self.addEventListener('activate', function(e) {
   console.log('[ServiceWorker] Activate');
   e.waitUntil(
     caches.keys().then(function(keyList) {
       return Promise.all(keyList.map(function(key) {
-        	if (key !== cacheName) {
+        	if (key !== cacheName && key !== dataCacheName) {
           		console.log('[ServiceWorker] Removing old cache', key);
           		return caches.delete(key);
         	}
@@ -44,12 +45,31 @@ self.addEventListener('activate', function(e) {
   );
   return self.clients.claim();
 });
-
+/* fetch */
 self.addEventListener('fetch', function(e) {
   console.log('[ServiceWorker] Fetch', e.request.url);
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
+	var dataUrl = 'https://query.yahooapis.com/v1/public/yql';
+  if (e.request.url.indexOf(dataUrl) > -1) {
+    /*
+     * When the request URL contains dataUrl, the app is asking for fresh
+     * weather data. In this case, the service worker always goes to the
+     * network and then caches the response. This is called the "Cache then
+     * network" strategy:
+     * https://jakearchibald.com/2014/offline-cookbook/#cache-then-network
+     */
+    e.respondWith(
+      caches.open(dataCacheName).then(function(cache) {
+        return fetch(e.request).then(function(response){
+          cache.put(e.request.url, response.clone());
+          return response;
+        });
+      })
+    );
+  }else{
+	  e.respondWith(
+	    caches.match(e.request).then(function(response) {
+	      return response || fetch(e.request);
+	    })
+	  );
+	}
 });
